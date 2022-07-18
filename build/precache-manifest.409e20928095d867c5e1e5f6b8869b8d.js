c7a5fcbc3bd533c00f83ee4e936bd192988d2c70
muhammadarmaghan <m.armaghan6@gmail.com>
2022-07-18 14:02:26 +0500

    third

---
+++ build/precache-manifest.409e20928095d867c5e1e5f6b8869b8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc26ae550425d03c536edd3abfb3690c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fafc5cd53eebb647cec2",
+    "url": "/static/css/2.25bda574.chunk.css"
+  },
+  {
+    "revision": "0437e5ba9ceb883bd3cf",
+    "url": "/static/css/main.84414c41.chunk.css"
+  },
+  {
+    "revision": "fafc5cd53eebb647cec2",
+    "url": "/static/js/2.93e67b45.chunk.js"
+  },
+  {
+    "revision": "3f1c3253327020ddb26385ba7d2cbefc",
+    "url": "/static/js/2.93e67b45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0437e5ba9ceb883bd3cf",
+    "url": "/static/js/main.4e57cd5b.chunk.js"
+  },
+  {
+    "revision": "9a301959369a59a3264e",
+    "url": "/static/js/runtime-main.f91afba1.js"
+  }
+]);
